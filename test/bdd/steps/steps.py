609318d08208a88d9f2d9d47aac0bd19235b2274
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 01:54:14 +0800

    Added needed import files

--- test/bdd/steps/steps.py
+++ test/bdd/steps/steps.py
@@ -1,14 +1,19 @@
 from lettuce import step
+from cdRental.cd_rental_app import CDStock, CUSTOMERLIST
+from cdRental.customer import Customer
+from cdRental.cd import CD
 
 
 @step(u'customer has id')
 def customer_has_id(step):
-    assert False, 'This step must be implemented'
+    customer = Customer(001, "Ron Daryl Magno")
+    CUSTOMERLIST.add_customer(customer)
 
 
 @step(u'CD has id')
 def cd_has_id(step):
-    assert False, 'This step must be implemented'
+    cd = CD("CD2", "Cloud Atlas", "No")
+    CDStock.add_cd(cd)
 
 
 @step(u'CD is not currently rented')

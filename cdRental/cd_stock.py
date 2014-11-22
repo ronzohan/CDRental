a1f203baa43f5f9bc541190f9e96b9405ad16211
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 00:03:29 +0800

    Added initial test cases

---
+++ cdRental/cd_stock.py
@@ -0,0 +1,9 @@
+class CDStock(object):
+    def __init__(self):
+        self.cds = {}
+
+    def add_cd(self, cd):
+        self.cds[cd.id] = cd
+        
+    def get_cd_data(self, id):
+        pass

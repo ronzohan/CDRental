8c34c7da943d7c433070d2702f14d0c501f39992
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 01:50:28 +0800

    Added test case on adding and getting customer data

--- test/unit/customer_list_test.py
+++ test/unit/customer_list_test.py
@@ -6,5 +6,21 @@ from cdRental.customer import Customer
 class TestCustomerList(unittest.TestCase):
     def test_add_customer_in_customer_list(self):
         customer1 = Customer(001, "Ron Daryl Magno")
+        customer2 = Customer(002, "Ron Daryl Magnos")
+
         customer_list = CustomerList()
+
         customer_list.add_customer(customer1)
+        customer_list.add_customer(customer2)
+
+        self.assertEqual(len(customer_list.list), 2)
+
+    def test_get_customer_data(self):
+        customer1 = Customer(001, "Ron Daryl Magno")
+
+        customer_list = CustomerList()
+
+        customer_list.add_customer(customer1)
+
+        self.assertEqual(customer_list.get_customer_data(001), customer1)
+        self.assertEqual(customer_list.get_customer_data(002), None)

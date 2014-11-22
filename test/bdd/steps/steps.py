d8e5afb16f63c40868ec4acb46e9e349020d895d
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 01:31:30 +0800

    Added all inital functions

---
+++ test/bdd/steps/steps.py
@@ -0,0 +1,26 @@
+from lettuce import step
+
+
+@step(u'Given customer has id')
+def given_customer_has_id(step):
+    assert False, 'This step must be implemented'
+
+
+@step(u'And CD has id')
+def and_cd_has_id(step):
+    assert False, 'This step must be implemented'
+
+
+@step(u'And CD is not currently rented')
+def and_cd_is_not_currently_rented(step):
+    assert False, 'This step must be implemented'
+
+
+@step(u'When I check out the cd')
+def when_i_check_out_the_cd(step):
+    assert False, 'This step must be implemented'
+
+
+@step(u'Then the CD is recorded as rented and a rental contract is printed')
+def then_the_cd_is_recorded_as_rented_and_a_rental_contract_is_printed(step):
+    assert False, 'This step must be implemented'

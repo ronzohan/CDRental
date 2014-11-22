580709d4ebf9dd439abfb34441f8bb588bc3704d
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-22 23:20:06 +0800

    Changed customer_id and customer_name to id and name

--- cdRental/customer.py
+++ cdRental/customer.py
@@ -1,5 +1,6 @@
 class Customer(object):
-    def __init__(self, customer_id, customer_name):
-        self.customer_id = customer_id
-        self.customer_name = customer_name
-        
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    

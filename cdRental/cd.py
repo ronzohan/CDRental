3f7ab3a09978ab8e34932ca1baa7af6555a59cd5
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 02:06:45 +0800

    added customer_id and rental_due attributes

--- cdRental/cd.py
+++ cdRental/cd.py
@@ -1,5 +1,7 @@
 class CD(object):
-    def __init__(self, id, title, rented):
+    def __init__(self, id, title, rented, customer_id = None, rental_due = None):
         self.id = id
         self.title = title
         self.rented = rented
+        self.customer_id = customer_id
+        self.rental_due = rental_due

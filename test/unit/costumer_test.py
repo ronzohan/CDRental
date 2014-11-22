5352bd5ecb8d24ad7e664fd8d1f2f6170cb6ccb7
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-22 23:21:34 +0800

    Added a test to check if object creation is successful

--- test/unit/costumer_test.py
+++ test/unit/costumer_test.py
@@ -1,9 +1,9 @@
 import unittest
 from cdRental.customer import Customer
+from cdRental.cd import CD
 
 
 class TestCostumer(unittest.TestCase):
-    def test_create_a_customer(self):
-        customer = Customer(001, "Ron Daryl Magno")
-
-    
+    def test_customer_returns_customer_data(self):
+        customer = Customer(001, "Ron Daryl")
+        self.assertEqual(customer.name, "Ron Daryl")

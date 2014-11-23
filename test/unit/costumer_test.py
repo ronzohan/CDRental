2660f2fcca8f9ac723fb196632490af4ea8c5315
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 23:24:33 +0800

    Added a test case where id parameter being passed is str

--- test/unit/costumer_test.py
+++ test/unit/costumer_test.py
@@ -7,3 +7,7 @@ class TestCostumer(unittest.TestCase):
     def test_customer_returns_customer_data(self):
         customer = Customer(001, "Ron Daryl")
         self.assertEqual(customer.name, "Ron Daryl")
+
+    def test_customer_id_is_string(self):
+        customer = Customer("001", "Ron Daryl")
+        self.assertEqual(customer.id, 001)

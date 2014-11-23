8f6a7e48acb29e6b84682861ff707e91308e370b
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 14:16:23 +0800

    Added rental_period attribute

--- cdRental/cd.py
+++ cdRental/cd.py
@@ -1,7 +1,8 @@
 class CD(object):
-    def __init__(self, id, title, rented, customer_id = None, rental_due = None):
+    def __init__(self, id, title, rented, customer_id=None, rental_period=None, rental_due=None):
         self.id = id
         self.title = title
         self.rented = rented
         self.customer_id = customer_id
+        self.rental_period = rental_period
         self.rental_due = rental_due

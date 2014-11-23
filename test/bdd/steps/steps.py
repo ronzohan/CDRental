bb253d3e7108b7705e76a2cf2f169ecc4c821d51
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 19:44:40 +0800

    Deleted step functions

--- test/bdd/steps/steps.py
+++ test/bdd/steps/steps.py
@@ -2,7 +2,7 @@ from lettuce import step, world
 from cdRental.cd_rental_app import CDLIST, CUSTOMERLIST, app
 from cdRental.customer import Customer
 from cdRental.cd import CD
-from nose.tools import assert_equal
+from nose.tools import assert_equal,assert_in
 from webtest import TestApp
 
 
@@ -40,21 +40,3 @@ def the_clerk_checks_out_the_cd_with_id_group1_to_customer_with_id_group2_on_gro
 
     assert_equal(world.form_response.status_code, 200)
 
-
-@step(u'CD with ID "([^"]*)" is recorded as rented')
-def cd_with_id_group1_is_recorded_as_rented(step, cd_id):
-    cd = CDLIST.get_cd_data(cd_id)
-    cd.set_rent()
-    CDLIST.cds[cd_id] = cd
-
-    assert_equal(CDLIST.get_cd_data(cd_id), cd)
-    assert_equal(CDLIST.get_cd_data(cd_id).rental_due, cd.rental_due)
-
-
-@step(u'And rental contract printed with customer ID "([^"]*)", customer name "([^"]*)", CD ID "([^"]*)", CD title "([^"]*)", and rental due on "([^"]*)"')
-def and_rental_contract_printed_with_customer_id_group1_customer_name_group2_cd_id_group3_cd_title_group4_and_rental_due_on_group5(step, 
-    customer_id, customer_name, cd_id, cd_title, cd_rental_due):
-
-    customer = CUSTOMERLIST.get_customer_data(customer_id)
-    cd = CDLIST.get_cd_data(cd_id)
-    

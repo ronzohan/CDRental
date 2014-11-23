7c4e3d982ed9a9bd13c703c6cbd08543a75049aa
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 23:36:28 +0800

    Removed clerk class, added function imports

--- test/bdd/steps/steps.py
+++ test/bdd/steps/steps.py
@@ -1,8 +1,7 @@
 from lettuce import step, world
-from cdRental.cd_rental_app import CDLIST, CUSTOMERLIST, app
+from cdRental.cd_rental_app import CDLIST, CUSTOMERLIST, app, generate_rental_contract, record_cd_as_rented
 from cdRental.customer import Customer
 from cdRental.cd import CD
-from cdRental.clerk import Clerk
 from nose.tools import assert_equal,assert_in
 from webtest import TestApp
 
@@ -28,13 +27,10 @@ def cd_with_id_group1_title_group2_has_a_rental_period_of_group3_days_and_isnot_
 @step(u'the clerk checks out the CD with ID "([^"]*)" to customer with ID "([^"]*)" on "([^"]*)"')
 def the_clerk_checks_out_the_cd_with_id_group1_to_customer_with_id_group2_on_group3(step, 
     cd_id, customer_id, date):
-
     world.browser = TestApp(app)
     world.response = world.browser.get('http://localhost:5000/')
-
-    assert_equal(world.response.status_code, 200)
-
     form = world.response.forms['checkout-form']
+    
     form['cd_id'] = cd_id
     form['customer_id'] = customer_id
     world.form_response = form.submit()
@@ -44,8 +40,8 @@ def the_clerk_checks_out_the_cd_with_id_group1_to_customer_with_id_group2_on_gro
 
 @step(u'CD with ID "([^"]*)" is recorded as rented with customer ID "([^"]*)"')
 def cd_with_id_group1_is_recorded_as_rented_with_customer_id_group2(step, cd_id, customer_id):
-    clerk = Clerk()
-    clerk.record_cd_as_rented(cd_id, customer_id)
+
+    record_cd_as_rented(cd_id, customer_id)
 
     assert_equal(CDLIST.get_cd_data(cd_id).customer_id, customer_id)
     assert_equal(CDLIST.get_cd_data(cd_id).rented, "Yes")
@@ -54,8 +50,8 @@ def cd_with_id_group1_is_recorded_as_rented_with_customer_id_group2(step, cd_id,
 @step(u'rental contract printed with customer ID "([^"]*)", customer name "([^"]*)", CD ID "([^"]*)", CD title "([^"]*)", and rental due on "([^"]*)"')
 def and_rental_contract_printed_with_customer_id_group1_customer_name_group2_cd_id_group3_cd_title_group4_and_rental_due_on_group5(step, 
     customer_id, customer_name, cd_id, cd_title, rental_due):
-    clerk = Clerk()
-    rental_contract = clerk.generate_rental_contract(cd_id)
+ 
+    rental_contract = generate_rental_contract(cd_id)
 
     assert_equal(rental_contract['CustomerID'], customer_id)
     assert_equal(rental_contract['CustomerName'], customer_name)

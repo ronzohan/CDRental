0a4d97f8aca605538c41f5c5f72b9309c32e9b62
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 18:23:29 +0800

    Added set_rent method

--- cdRental/checkout_cd.py
+++ cdRental/checkout_cd.py
@@ -7,8 +7,9 @@ class CheckoutCD(object):
         if type(customer) == Customer and type(cd) == CD:
             self.customer = customer
             self.cd = cd
-
-            self.cd.set_rent(customer)
         else:
             raise TypeError("Customer must be of {} and cd must of be".format\
                             (type(customer), type(cd)))
+
+    def set_rent(self):
+        self.cd.set_rent(self.customer)

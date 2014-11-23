593068f1005035272ab4cdf0546ee776e02d244c
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-23 19:43:04 +0800

    Added record_cd_as_rented method

--- cdRental/clerk.py
+++ cdRental/clerk.py
@@ -1,2 +1,7 @@
+from cdRental.cd_rental_app import CDLIST
+
+
 class Clerk(object):
-    pass
+    def record_cd_as_rented(self, cd_id, customer_id):
+        cd = CDLIST.get_cd_data(cd_id)
+        cd.set_rent(customer_id)

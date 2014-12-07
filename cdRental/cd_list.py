ff0fc5843143533829ca3060e1a266adc1286ff3
Ron Daryl Magno <ron.zohan@yahoo.com>
2014-12-07 21:56:05 +0800

    Deleted files not needed since im using orm in this branch

--- cdRental/cd_list.py
+++
@@ -1,9 +0,0 @@
-class CDList(object):
-    def __init__(self):
-        self.cds = {}
-
-    def add_cd(self, cd):
-        self.cds[cd.id] = cd
-
-    def get_cd_data(self, id):
-        return self.cds.get(id)
--- cdRental/customer.py
+++
@@ -1,7 +0,0 @@
-class Customer(object):
-    def __init__(self, id, name):
-        if type(id) == str:
-            id = int(id)
-
-        self.id = id
-        self.name = name
